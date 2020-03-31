60bada3ff58e87eabff643f751c1e586b74352a8
UnGu Zartex <yentl.kinoo@student.kuleuven.be>
2020-03-31 11:56:12 +0200

    Updated api

--- src/main/java/GameWorldAPI/GameWorldType/GameWorldType.java
+++ src/main/java/GameWorldAPI/GameWorldType/GameWorldType.java
@@ -2,6 +2,7 @@ package GameWorldAPI.GameWorldType;
 
 import GameWorldAPI.GameWorld.GameWorld;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -22,6 +23,13 @@ public interface GameWorldType {
      * @return a List of Predicate objects.
      */
     List<Predicate> getAllPredicates();
+
+    /**
+     * Loads a GameWorld from a file
+     * @param file the file to load in
+     * @return a GameWorld that corresponds to the information in the file
+     */
+    GameWorld loadFromFile(File file);
 
     /**
      * Create a new instance of this GameWorldType
